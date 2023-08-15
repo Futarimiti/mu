966771e347f0acdedc77852d7468b456cbbdb3c3
Futar <Futarimiti@163.com>
2023-08-15 17:50:43 +0800

    add editor

---
+++ app/Editor.hs
@@ -0,0 +1,20 @@
+module Editor (Editor(..), shellEditor) where
+
+import           Control.Applicative       (Alternative (..))
+import           Control.Monad.Trans.Maybe (MaybeT (..))
+import           System.Environment        (lookupEnv)
+import           System.Process            (callProcess)
+
+-- some programme able to edit a file
+-- at a given filepath
+newtype Editor = Editor { edit :: FilePath -> IO () }
+
+shellEditor :: MaybeT IO Editor
+shellEditor = do cmd <- shellEditorCommand
+                 return Editor { edit = callProcess cmd . pure }
+
+-- | Try to get a shell editor from VISUAL & EDITOR
+shellEditorCommand :: MaybeT IO FilePath
+shellEditorCommand = MaybeT $ do visual <- lookupEnv "VISUAL"
+                                 editor <- lookupEnv "EDITOR"
+                                 return $ visual <|> editor
