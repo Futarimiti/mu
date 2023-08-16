8e5cc3ee46903a823bbe432a936d020d5a38c19c
Futar <Futarimiti@163.com>
2023-08-16 10:34:07 +0800

    init function to run commands

--- app/Commands.hs
+++ app/Commands.hs
@@ -1,9 +1,28 @@
-module Commands (MuCommand(..)) where
+{-# LANGUAGE LambdaCase #-}
 
+module Commands (runCommand, MuCommand(..)) where
+
+import           Config  (Config)
 import           Lib
+import           Prelude hiding (log)
 
 -- | Functionalities parsed from commandline args
 data MuCommand = Play [SongName]  -- | Play songs sequentially
                | Shuffle          -- | Shuffle through the library
                | Update           -- | Update library
                deriving (Show, Eq, Read)
+
+runCommand :: Config -> MuCommand -> IO ()
+runCommand c = \case (Play songs) -> seqPlay songs
+                     Shuffle -> shuffle c
+                     Update -> update c
+
+update :: Config -> IO ()
+update = const $ log "update: TODO"
+
+shuffle :: Config -> IO ()
+shuffle = const $ log "shuffle: TODO"
+
+seqPlay :: [SongName] -> IO ()
+seqPlay = const $ log "seqPlay: TODO"
+
