10871c15edc07afe51dfa852b796042b0a126113
Futar <Futarimiti@163.com>
2023-08-16 10:33:07 +0800

    using messages resource

---
+++ app/Messages.hs
@@ -0,0 +1,17 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
+module Messages where
+
+import           Data.Text    (Text, pack)
+import           Dhall        (FromDhall, auto, input)
+import           GHC.Generics (Generic)
+import           Lib
+import           Paths_mu     (getDataFileName)
+
+data Messages = Messages { cannotInferDefaultPlayerByOS :: OS -> Text
+                         , cannotInferMusicDir          :: Text
+                         , mustManuallySetDownloader    :: Text
+                         } deriving (Generic, FromDhall)
+
+messages :: IO Messages
+messages = getDataFileName "messages.dhall" >>= input auto . pack
