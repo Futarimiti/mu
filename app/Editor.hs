a7852589e4f2c6fa77f8897c5ada6ee35f88d212
Futar <Futarimiti@163.com>
2024-02-15 16:16:48 +0000

    remove unnecessary exports

--- app/Editor.hs
+++ app/Editor.hs
@@ -1,26 +1,5 @@
-module Editor (Editor(..), shellEditor, vim, notepad) where
-
-import           Control.Applicative       (Alternative (..))
-import           Control.Monad.Trans.Maybe (MaybeT (..))
-import           System.Environment        (lookupEnv)
-import           System.Process            (callProcess)
+module Editor (Editor(..)) where
 
 -- some programme able to edit a file
 -- at a given filepath
 newtype Editor = Editor { edit :: FilePath -> IO () }
-
-shellEditor :: MaybeT IO Editor
-shellEditor = do cmd <- shellEditorCommand
-                 return Editor { edit = callProcess cmd . pure }
-
--- | Try to get a shell editor from VISUAL & EDITOR
-shellEditorCommand :: MaybeT IO FilePath
-shellEditorCommand = MaybeT $ do visual <- lookupEnv "VISUAL"
-                                 editor <- lookupEnv "EDITOR"
-                                 return $ visual <|> editor
-
-vim :: Editor
-vim = Editor { edit = callProcess "vim" . pure }
-
-notepad :: Editor
-notepad = Editor { edit = callProcess "editor" . pure }
