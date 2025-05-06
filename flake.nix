037861dc2ab3e5462e91575e7c5b9161cd155bd0
Carman Fu <futarimiti@163.com>
2025-05-06 15:02:11 +0100

    nix: init flake

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "mu the music player";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        haskellPackages = pkgs.haskell.packages.ghc984;
+      in
+      {
+        packages = rec {
+          mu = haskellPackages.developPackage { root = ./.; };
+          default = mu;
+        };
+        apps = rec {
+          mu = flake-utils.lib.mkApp { drv = self.packages.${system}.mu; };
+          default = mu;
+        };
+        devShells = {
+          default = haskellPackages.shellFor {
+            packages = ps: [ (ps.callCabal2nix "mu" ./. { }) ];
+            nativeBuildInputs = with haskellPackages; [
+              haskell-language-server
+              cabal-install
+              hpack
+            ];
+            withHoogle = false;
+          };
+        };
+      }
+    );
+}
